71a58ab0883612ae17a5d6fa6668d4410a228891
Ishant Srivastava <ishantsri1827gmail@gmail.com>
2026-07-28 23:01:37 -0700

    latest

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ if not api_key:
 
 genai.configure(api_key=api_key)
 
-model = genai.GenerativeModel("gemini-3.6-flash")
+model = genai.GenerativeModel("gemini-flash-latest")
 
 # ===========================
 # Page Configuration
